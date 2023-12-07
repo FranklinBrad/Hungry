68126b8f02ef3aacb9f39bef06729235d1249080
FranklinBrad <franklin1jbradley@gmail.com>
2023-12-06 21:22:42 -0600

    added contact me

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,23 @@
+import Form from 'react-bootstrap/Form';
+
+
+
 function Contact(){
     return (
         <>
         
     <h1>Contact Me!</h1>
+   <div>
+    <h2>send me an email here</h2>
+   <form action="https://formsubmit.co/franklin1jbradley@gmail.com" method="POST">
+     <input type="text" name="name" required/>
+     <input type="email" name="email" required/>
+
     
+     <button type="submit">Send</button>
+  </form>
+   </div>
+
 
     </>
     );
