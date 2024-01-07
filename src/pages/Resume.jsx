2276ef6f0637e6a642e819b39e08b245019e812c
FranklinBrad <franklin1jbradley@gmail.com>
2024-01-07 16:04:21 -0600

    new resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,4 @@
-import Resume1 from "../images/image0.jpeg";
+ import Resume2 from "../images/resume2.png"
 import Button from "react-bootstrap/Button";
 function Resume() {
   return (
@@ -7,10 +7,10 @@ function Resume() {
         <h1>My Resume</h1>
       </div>
       <div className="resdoc">
-        <img className="resumepic" src={Resume1}></img>
+        <img className="resumepic" src={Resume2}></img>
       </div>
       <div className="download">
-        <a href={Resume1} download="resume">
+        <a href={Resume2} download="resume">
           <Button variant="secondary">Download Resume</Button>{" "}
         </a>
       </div>
